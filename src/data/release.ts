e06469614c5bae84da45f2c8b35b3c22f74bc3b5
Jean-Pierre Yin <jpierre.yin@gmail.com>
2026-05-30 02:02:47 +0200

    Update website for Canopi v0.5.0

---
+++ src/data/release.ts
@@ -0,0 +1,14 @@
+export const CANOPI_VERSION = '0.5.0';
+export const CANOPI_RELEASE_TAG = `v${CANOPI_VERSION}`;
+export const CANOPI_RELEASE_URL = `https://github.com/naejin/canopi/releases/tag/${CANOPI_RELEASE_TAG}`;
+export const CANOPI_RELEASES_URL = 'https://github.com/naejin/canopi/releases';
+export const CANOPI_DOWNLOAD_BASE_URL = `https://github.com/naejin/canopi/releases/download/${CANOPI_RELEASE_TAG}`;
+
+export const CANOPI_DOWNLOADS = {
+  linuxAppImage: `${CANOPI_DOWNLOAD_BASE_URL}/Canopi_${CANOPI_VERSION}_amd64.AppImage`,
+  linuxDeb: `${CANOPI_DOWNLOAD_BASE_URL}/Canopi_${CANOPI_VERSION}_amd64.deb`,
+  macAppleSiliconDmg: `${CANOPI_DOWNLOAD_BASE_URL}/Canopi_${CANOPI_VERSION}_aarch64.dmg`,
+  macIntelDmg: `${CANOPI_DOWNLOAD_BASE_URL}/Canopi_${CANOPI_VERSION}_x64.dmg`,
+  windowsSetupExe: `${CANOPI_DOWNLOAD_BASE_URL}/Canopi_${CANOPI_VERSION}_x64-setup.exe`,
+  windowsMsi: `${CANOPI_DOWNLOAD_BASE_URL}/Canopi_${CANOPI_VERSION}_x64_en-US.msi`,
+} as const;
