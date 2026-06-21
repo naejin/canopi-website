729b1b18d87307fa0e84c9a47167c8f2a37ee9a3
Jean-Pierre Yin <jpierre.yin@gmail.com>
2026-06-21 21:08:53 +0200

    Update website for Canopi v0.8.0

--- src/data/release.ts
+++ src/data/release.ts
@@ -1,5 +1,5 @@
-export const CANOPI_VERSION = '0.7.1';
-export const CANOPI_RELEASE_DATE = '2026-06-16';
+export const CANOPI_VERSION = '0.8.0';
+export const CANOPI_RELEASE_DATE = '2026-06-21';
 export const CANOPI_RELEASE_TAG = `v${CANOPI_VERSION}`;
 export const CANOPI_RELEASE_URL = `https://github.com/naejin/canopi/releases/tag/${CANOPI_RELEASE_TAG}`;
 export const CANOPI_RELEASES_URL = 'https://github.com/naejin/canopi/releases';
